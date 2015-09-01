de9f12039feef9469f4aaed5502cd444624472fc
Richard Snider <richardnsnider@gmail.com>
2015-09-01 16:47:45 -0700

    Preparing to implement Unity game project.

--- src/mogvum/Game.java
+++
@@ -1,9 +0,0 @@
-package mogvum;
-
-class Game {
-
-	
-	
-	public void run() {
-	}
-}
--- src/mogvum/Main.java
+++
@@ -1,9 +0,0 @@
-package mogvum;
-
-class Main {
-		public static void main(String[] args) {
-			Game g = new Game();
-			g.run();
-			System.exit(0);
-		}
-}
