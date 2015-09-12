54f1540c299b5a63b41e1788788d52c3c20ee30f
Richard Snider <richardnsnider@gmail.com>
2015-09-12 16:12:33 -0700

    Add terrain type.

---
+++ Assets/Scripts/Enumerables/TerrainType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Enumerables
+{
+    public enum TerrainType
+    {
+        Normal,
+        Ocean,
+        Island,
+        Plains,
+        Forest,
+        Swamp,
+        Lake,
+        Coast,
+        Mountain,
+        Fog
+    }
+}
