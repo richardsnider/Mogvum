bacd7fbd47e37cd240bd3beb96a946c64c0d3c81
Richard Snider <richardnsnider@gmail.com>
2015-09-18 10:00:10 -0700

    Create project for Unit Testing.

---
+++ Tests/GameTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assets.Scripts;
+
+namespace Tests
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void CreateGame()
+        {
+            //var game = new Game("Test Game");
+
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
