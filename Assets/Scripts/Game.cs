161e6975de30377c132066074693e20266b9af90
Richard Snider <richardnsnider@gmail.com>
2015-09-22 21:52:38 -0700

    LogEvents for tests.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assets.Scripts.Testing;
+using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Assets.Scripts
@@ -9,18 +11,20 @@ namespace Assets.Scripts
         public Guid Id;
         private DateTime saveDate;
         public float GameVersion, PatchNumber;
+        public ICollection<LogEvent> LogEvents;
 
         public string Name;
         private World world;
 
         public Game(Guid id = new Guid(), DateTime? saveDate = null, 
-            float gameVersion = 0, short patchNumber = 0,
+            float gameVersion = 0, short patchNumber = 0, ICollection<LogEvent> logEvents = null,
             string name = null, World world = null)
         {
             this.Id = id;
             this.saveDate = saveDate ?? DateTime.Now;
             this.GameVersion = gameVersion;
             this.PatchNumber = patchNumber;
+            this.LogEvents = logEvents ?? new List<LogEvent>();
 
             this.Name = name;
             this.world = world ?? new World(this);
---
+++ Assets/Scripts/Testing/Tests.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Utilities;
+using UnityEngine.Assertions;
+
+namespace Assets.Scripts.Testing
+{
+    public class Tests
+    {
+        public void GamePersistence()
+        {
+            string gameName = "Test Case Game Name";
+            Game game = new Game(name: gameName);
+
+            Persistence.Save(game);
+
+            var loadedGame = Persistence.Load(game.Id);
+
+            Assert.AreEqual(loadedGame.Name, gameName);
+        }
+    }
+}
