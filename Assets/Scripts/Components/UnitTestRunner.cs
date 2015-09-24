abc8bf7396d42a9c189704964915123bdba87721
Richard Snider <richardnsnider@gmail.com>
2015-09-24 10:07:22 -0700

    Add UnitTestRunner component script.

---
+++ Assets/Scripts/Components/UnitTestRunner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Assets.Scripts.Testing;
+
+namespace Assets.Scripts.Components
+{
+    class UnitTestRunner : MonoBehaviour
+    {
+        void Start()
+        {
+            Tests tests = new Tests();
+
+            tests.GamePersistence();
+
+            Debug.Log("Finished tests");
+        }
+    }
+}
