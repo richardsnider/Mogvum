5a590532926f8e8f71a27eac66ccc12d0f0709c3
Richard Snider <richardnsnider@gmail.com>
2015-10-03 09:01:21 -0700

    Basic refactoring for RegionCube.

--- Assets/Scripts/Regions/RegionCube.cs
+++ Assets/Scripts/Regions/RegionCube.cs
@@ -10,68 +10,75 @@ namespace Assets.Scripts.Regions
     [Serializable]
     public class RegionCube
     {
-        private Region region;
-        private int x, y, z;
-        private Matter matter;
-        private ICollection<IEffect> effects;
-        private ICollection<Character> occupants;
-        private Inventory inventory;
-        private int cover, lighting;
+        private Region Region { get; set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+        public Matter Matter { get; private set; }
+        public ICollection<IEffect> Effects { get; private set; }
+        public ICollection<Character> Occupants { get; private set; }
+        public Inventory Inventory { get; private set; }
+        public int Cover { get; private set; }
+        public int Lighting { get; private set; }
 
         public RegionCube(Region region, int x, int y, int z, Matter matter = Matter.Vacuum, ICollection<IEffect> effects = null,
             ICollection<Character> occupants = null, Inventory inventory = null, int cover = 0, int lighting = 100)
         {
-            this.region = region;
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.matter = matter;
-            this.effects = effects ?? new List<IEffect>();
-            this.occupants = occupants ?? new List<Character>();
-            this.inventory = inventory ?? new Inventory(0, 0, true);
-            this.cover = cover;
-            this.lighting = lighting;
+            Region = region;
+            X = x;
+            Y = y;
+            Z = z;
+            Matter = matter;
+            Effects = effects ?? new List<IEffect>();
+            Occupants = occupants ?? new List<Character>();
+            Inventory = inventory ?? new Inventory(0, 0, true);
+            Cover = cover;
+            Lighting = lighting;
         }
 
         public bool AddEffect(IEffect effect)
         {
-            if (!effects.Contains(effect))
+            if (!Effects.Contains(effect))
             {
-                effects.Add(effect);
+                Effects.Add(effect);
                 return true;
             }
-            else return false;
+
+            return false;
         }
 
         public IEffect RemoveEffect(IEffect effect)
         {
-            if (effects.Contains(effect))
+            if (Effects.Contains(effect))
             {
-                effects.Remove(effect);
+                Effects.Remove(effect);
                 return effect;
             }
-            else return null;
+
+            return null;
         }
 
         public bool AddOccupant(Character character)
         {
-            if ((character.Stats.Size <= CharacterSize.Tiny || occupants.Count == 0)
-                && !occupants.Contains(character))
+            if ((character.Stats.Size <= CharacterSize.Tiny || Occupants.Count == 0)
+                && !Occupants.Contains(character))
             {
-                occupants.Add(character);
+                Occupants.Add(character);
                 return true;
             }
-            else return false;
+
+            return false;
         }
 
         public Character RemoveOccupant(Character character)
         {
-            if (occupants.Contains(character))
+            if (Occupants.Contains(character))
             {
-                occupants.Remove(character);
+                Occupants.Remove(character);
                 return character;
             }
-            else return null;
+
+            return null;
         }
     }
 }
