829d18a67ef34d5aece2c9cb35a2f95f9e38b757
Richard Snider <richardnsnider@gmail.com>
2015-10-03 11:04:59 -0700

    Refactor faction class.

--- Assets/Scripts/Players/Faction.cs
+++ Assets/Scripts/Players/Faction.cs
@@ -7,20 +7,21 @@ namespace Assets.Scripts.Players
     [Serializable]
     public class Faction
     {
-        private World world;
-        private string name;
-        private ICollection<Character> characters;
+        private World World { get; set; }
+        public string Name { get; private set; }
+        public ICollection<Character> Characters { get; private set; }
 
         public Faction(World world, string name = null, ICollection<Character> characters = null)
         {
-            this.name = name ?? "DefaultFactionName"; //Need to add a generator!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            this.characters = characters ?? new List<Character>();
+            World = world;
+            Name = name ?? Utilities.NameGenerator.CreateNames()[1];
+            Characters = characters ?? new List<Character>();
         }
 
         public void AddCharacter(Character character)
         {
-            if(!characters.Contains(character))
-                characters.Add(character);
+            if(!Characters.Contains(character))
+                Characters.Add(character);
         }
     }
 }
