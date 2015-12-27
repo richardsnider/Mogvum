6719202eeaaceb4ea3d535a351613b0fec78f173
Richard Snider <richardnsnider@gmail.com>
2015-12-27 00:57:59 -0800

    Update to make region cube an effect target.

--- Assets/Scripts/Regions/RegionCube.cs
+++ Assets/Scripts/Regions/RegionCube.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Scripts.Characters;
 using Assets.Scripts.Effects;
 using Assets.Scripts.Items;
@@ -7,14 +8,14 @@ using Assets.Scripts.Items;
 namespace Assets.Scripts.Regions
 {
     [Serializable]
-    public class RegionCube : IHasInventory
+    public class RegionCube : IHasInventory, IEffectTarget<RegionCube>
     {
         private Region Region { get; set; }
         public int X { get; private set; }
         public int Y { get; private set; }
         public int Z { get; private set; }
         public Matter Matter { get; private set; }
-        public ICollection<IEffect> Effects { get; private set; }
+
         public ICollection<Character> Occupants { get; private set; }
         public Inventory Inventory { get; private set; }
         public int Cover { get; private set; }
@@ -28,33 +29,10 @@ namespace Assets.Scripts.Regions
             Y = y;
             Z = z;
             Matter = matter;
-            Effects = effects ?? new List<IEffect>();
             Occupants = occupants ?? new List<Character>();
             Inventory = inventory ?? new Inventory(this);
             Cover = cover;
             Lighting = lighting;
-        }
-
-        public bool AddEffect(IEffect effect)
-        {
-            if (!Effects.Contains(effect))
-            {
-                Effects.Add(effect);
-                return true;
-            }
-
-            return false;
-        }
-
-        public IEffect RemoveEffect(IEffect effect)
-        {
-            if (Effects.Contains(effect))
-            {
-                Effects.Remove(effect);
-                return effect;
-            }
-
-            return null;
         }
 
         public bool AddOccupant(Character character)
@@ -83,6 +61,17 @@ namespace Assets.Scripts.Regions
         public RegionCube Cube
         {
             get { return this; }
+        }
+
+        public ICollection<IEffect<RegionCube>> Effects { get; set; }
+        public void AddEffect(IEffect<RegionCube> effect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveEffect(IEffect<RegionCube> effect)
+        {
+            throw new NotImplementedException();
         }
     }
 }
